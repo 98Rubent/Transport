2005370684514f7673e4d95f658eb41d8a622880
98Rubent <torrestenrique98@gmail.com>
2022-06-17 03:08:46 -0600

    Asi estaba

--- Transport/Startup.cs
+++ Transport/Startup.cs
@@ -75,7 +75,12 @@ namespace Transport
                 app.UseDeveloperExceptionPage();
                 app.UseMigrationsEndPoint();
             }
-            
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
